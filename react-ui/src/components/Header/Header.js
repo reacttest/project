bed3e71024dfd3b3e4c76d858e682b16102d7f1d
Adrian Karas <akaras@pgs-soft.com>
2018-01-12 13:11:03 +0100

    new ddd

--- react-ui/src/components/Header/Header.js
+++ react-ui/src/components/Header/Header.js
@@ -14,17 +14,20 @@ class Header extends Component {
         const {activeItem} = this.state;
 
         return (
-                <Menu inverted style={{'borderRadius': 0, height: this.props.headerHeight}} fixed='top'>
-                    <Menu.Item as={Link} to="/">Home</Menu.Item>
-                    <Menu.Item as={Link} to="/projects">Projects</Menu.Item>
-                    <Menu.Item as={Link} to="/groups">Groups</Menu.Item>
-                    <Menu.Menu position='right'>
-                        <Menu.Item>
-                            <Input icon='search' placeholder='Search...' />
-                        </Menu.Item>
-                        <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.handleItemClick} />
-                    </Menu.Menu>
-                </Menu>
+                <div>
+                    <Menu inverted style={{'borderRadius': 0, height: this.props.headerHeight}} fixed='top'>
+                        <Menu.Item as={Link} to="/">Home</Menu.Item>
+                        <Menu.Item as={Link} to="/projects">Projects</Menu.Item>
+                        <Menu.Item as={Link} to="/groups">Groups</Menu.Item>
+                        <Menu.Menu position='right'>
+                            <Menu.Item>
+                                <Input icon='search' placeholder='Search...' />
+                            </Menu.Item>
+                            <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.handleItemClick} />
+                        </Menu.Menu>
+                    </Menu>
+                    {this.props.children}
+                </div>
         )
     }
 }
--- server/app.js
+++ server/app.js
@@ -27,15 +27,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
-// // Enable CORS from client-side
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-//     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
-//     res.header('Access-Control-Allow-Credentials', 'true');
-//     next();
-// });
-
 // Require static assets from public folder
 // app.use(express.static(path.join(__dirname, 'public')));
 
